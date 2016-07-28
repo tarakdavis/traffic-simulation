163ceff73ef1fab582749aaa7bb79dfa2ce520c8
Tara Davis <tkalindavis@gmail.com>
2016-07-28 16:48:37 -0400

    deleted a file

--- road_rage.py
+++ road_rage.py
@@ -1,1 +1,25 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import statistics
+
+
+class Car:
+    def __init__(self, max_speed=33.333, size=5, accel=2, location=0):
+        self.max_speed = max_speed
+        self.size = size
+        self.speed = speed
+        self.location = location
+
+    def is_car_moving(self):
+        # will velocity carry you into a car? if yes, False
+        slow = randint(0, 9)
+        if slow == 0:
+            return accel -= 2
+
+
+
+class Road:
+    def length(self, length=1000):
+
+
+class Simulation:
