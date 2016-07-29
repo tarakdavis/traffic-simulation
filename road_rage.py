2f13dedf604b229c1d53a8f7906020c9efca7af5
Tara Davis <tkalindavis@gmail.com>
2016-07-29 11:45:53 -0400

    Updated classes.

--- road_rage.py
+++ road_rage.py
@@ -4,22 +4,43 @@ import statistics
 
 
 class Car:
-    def __init__(self, max_speed=33.333, size=5, accel=2, location=0):
+    def __init__(self, max_speed=33.33, size=5, accel=2, location=0, slow_percentage=0.1):
         self.max_speed = max_speed
         self.size = size
-        self.speed = speed
+        self.accel = accel
         self.location = location
+        self.slow_percentage = slow_percentage
 
-    def is_car_moving(self):
-        # will velocity carry you into a car? if yes, False
-        slow = randint(0, 9)
-        if slow == 0:
-            return accel -= 2
+    """Car accelerates normally."""
+    def accelerate_car(self):
 
+
+    """Car matches speed of car in front if about to hit."""
+    def is_car_close(self):
+
+    """Car randomly slows 2 m/s at 10% chance"""
+    def is_car_slowing(self):
+        if random.random() = self.slow_percentage
+            return self.accel -= 2
 
 
 class Road:
-    def length(self, length=1000):
+    def __init__(self, length=1000, cars=30, list_of_cars):
+        self.length = length
+        self.cars = cars
+        self.list_of_cars = []
+
+    """Place cars on road"""
+    def populate_cars(self):
+        total_length = Car.size * self.cars
+        return round(self.length / total_lenth, 2)
+
+    def position_car():
+
 
 
 class Simulation:
+    def __init__(self, max_time=60):
+        self.max_time = max_time
+
+    def run_simulation():
