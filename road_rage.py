af4180d835a8a241e05e6b52d54ddfff0c97e41f
Tara Davis <tkalindavis@gmail.com>
2016-07-29 17:36:43 -0400

    Updated Car class.

--- road_rage.py
+++ road_rage.py
@@ -4,25 +4,29 @@ import statistics
 
 
 class Car:
-    def __init__(self, max_speed=33.33, size=5, accel=2, location=0, slow_percentage=0.1, position):
-        self.max_speed = max_speed
-        self.size = size
-        self.accel = accel
-        self.location = location
-        self.slow_percentage = slow_percentage
-        self.position = np.array([position, position + 5])
+    def __init__(self, position, car_in_front=None):
+        self.max_speed = 33.33
+        self.size = 5
+        self.speed = 0
+        self.accel = 2
+        self.slow_percentage = 0.1
+        self.position = np.array([position, position + self.size])
 
-    """Car accelerates normally."""
+# Car accelerates normally.
     def accelerate_car(self):
+        self.speed += self.accel
 
+# Car matches speed of car in front if about to hit.
+    def is_car_in_front(self, car_in_front):
+        distance = car_in_front.position[0] - self.position[1]
+        if distance <= self.speed:
+            if self.speed >= car_in_front.speed:
+                self.speed == car_in_front.speed
 
-    """Car matches speed of car in front if about to hit."""
-    def is_car_close(self):
-
-    """Car randomly slows 2 m/s at 10% chance"""
+# Car randomly slows 2 m/s at 10% chance
     def is_car_slowing(self):
-        if random.random() = self.slow_percentage
-            return self.accel -= 2
+        if random.random() == self.slow_percentage:
+            self.speed -= self.accel
 
 
 class Road:
